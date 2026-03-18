83cbcbb00cf139a20c4b33666316f07abcdcdcd1
Dmytro Baklai <d.n.baklai@gmail.com>
2026-03-18 15:02:03 +0200

    fix

--- packages/app/vite.config.js
+++ packages/app/vite.config.js
@@ -7,7 +7,7 @@ import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/app/',
   plugins: [
     vue(),
     vueJsx(),
